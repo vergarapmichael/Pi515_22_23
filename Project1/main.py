997039385090ff304c453fce2313af7e1cf7b241
vergarapal <115191505+vergarapmichael@users.noreply.github.com>
2022-11-10 22:37:18 +0000

    everything

--- Project1/main.py
+++ Project1/main.py
@@ -8,6 +8,7 @@ while keepAsking:
     if num.isdigit():
         print("Let's play!")
         num = int(num)
+        keepAsking = False
         # something about the loop condition
     else:
         print("Invalid input. Try again.")
@@ -20,10 +21,12 @@ while guess != secret:
     guess = input("Type a number between 1 and " + str(num) + ": ")
     if guess.isdigit():
         guess = int(guess)
-    if False: # can't always be wrong
+    if guess == secret: # can't always be wrong
         print("You got it!")
+        count += 1
     else:
         print("Try again.")
+        count += 1
         # what else?
 
 if count == 1:
